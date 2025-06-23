c56039d1d8ea8142f0ebf04895fdb7d0661d348b
Osamikoyo <v.pylayev@mail.ru>
2025-06-23 08:09:38 +0500

    add get_requests entity

--- internal/entity/get_requests.go
+++ internal/entity/get_requests.go
@@ -10,4 +10,18 @@ type (
 		Value string `json:"value"`
 		Count int    `json:"count"`
 	}
+
+	Create[T any] struct {
+		Payload T `json:"payload"`
+	}
+
+	Update struct{
+		ID string `json:"id"`
+		Key string `json:"key"`
+		Value string `json:"value"`
+	}
+
+	Delete struct{
+		ID string `json:"id"`
+	}
 )
