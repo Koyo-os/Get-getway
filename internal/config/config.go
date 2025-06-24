ada1215a647e660f4a4035b0ef142724b38f39dd
Osamikoyo <v.pylayev@mail.ru>
2025-06-24 16:41:26 +0500

    update config

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,16 +7,16 @@ type (
 		Urls      map[string]string
 		Exchanges map[string]string
 		Queue     map[string]string
+		Port      int
 	}
 )
 
-
 func NewConfig() *Config {
 	config := &Config{
-        Urls: make(map[string]string),
-        Exchanges: make(map[string]string),
-        Queue: make(map[string]string),
-    }
+		Urls:      make(map[string]string),
+		Exchanges: make(map[string]string),
+		Queue:     make(map[string]string),
+	}
 
 	config.Exchanges["vote"] = "vote"
 	config.Exchanges["poll"] = "poll"
@@ -26,6 +26,13 @@ func NewConfig() *Config {
 	maps.Copy(config.Exchanges, config.Queue)
 
 	config.Exchanges["self"] = "self"
-	
-    return config
+
+	config.Urls["answer"] = "localhost:50051"
+	config.Urls["form"] = "localhost:50052"
+	config.Urls["vote"] = "localhost:50053"
+	config.Urls["poll"] = "localhost:50054"
+
+	config.Port = 50050
+
+	return config
 }
